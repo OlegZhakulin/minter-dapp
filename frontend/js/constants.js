f7517270b6dc1374084bef3271e6e7001e251ce4
Oleg Zhakulin <nft.oleh@gmail.com>
2022-05-30 18:48:22 +0300

    update sity

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x2f131C2A3aa04e920d68A84008f434560Bb4A18f"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
